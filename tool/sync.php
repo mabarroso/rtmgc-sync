cc9392bcbdebeab06456e1c1b4b2234dac283e40
mabarroso <mabarroso@mabarroso.com>
2013-10-15 16:54:29 +0200

    Sync for tests accounts

---
+++ tool/sync.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Sync.php
+ * Sync test application
+ *
+ * PHP version 5.2
+ *
+ * @category  RTMGC
+ * @package   Sync
+ * @author    mabarroso <mabarroso@mabarroso.com>
+ * @copyright 2013 mabarroso.com
+ * @license   Apache 2 License http://www.apache.org/licenses/LICENSE-2.0.html
+ * @version   GIT: $Id$
+ * @link      http://www.mabarroso.com
+ * @since     File available since Release 0.1
+ */
+
+require_once '../src/Sync.php';
+
+/**
+ * Sync
+ *
+ * @category  RTMGC
+ * @package   Sync
+ * @author    mabarroso <mabarroso@mabarroso.com>
+ * @copyright 2013 mabarroso.com
+ * @license   Apache 2 License http://www.apache.org/licenses/LICENSE-2.0.html
+ * @version   GIT: $Id$
+ * @link      http://www.mabarroso.com
+ * @since     File available since Release 0.1
+ */
+
+$sync = new Sync('sync.json');
+$sync->load();
+$sync->connect();
+
