6e0f57a24677a357a56d71bdab5214df9c4a2de7
mabarroso <mabarroso@mabarroso.com>
2013-10-17 19:15:00 +0200

    Fix Exception get message

--- src/Sync/RememberTheMilk.php
+++ src/Sync/RememberTheMilk.php
@@ -78,7 +78,7 @@ class RememberTheMilk
             $this->_rtm->getService(Rtm::SERVICE_AUTH)->checkToken();
         } catch(Exception $e) {
             $this->_rtm = false;
-            throw new Exception($e->message);
+            throw new Exception($e);
         }
     }
 
